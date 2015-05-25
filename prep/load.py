1e1257e2e4d7879515bdbd8d467635f9a3d2e5ec
Sebastian <sebastian@s-klamt.de>
2015-05-25 16:36:04 +0200

    Added data and a script to load the data as a numpy array

---
+++ prep/load.py
@@ -0,0 +1,37 @@
+import numpy as np
+import datetime
+from datetime import timedelta
+
+def load_to_arrays(infile):
+    f=open(infile,"r")
+    arrays=[]
+    date=datetime.datetime.strptime(infile.split("/")[-1][0:10],"%Y-%m-%d")
+    tag=""
+    pos=f.tell()
+    nextline=f.readline()
+    f.seek(pos)
+    splitline=nextline.split(" ")
+    stime=timedelta(hours=int(splitline[0]),minutes=int(splitline[1]),seconds=int(splitline[2]),microseconds=int(splitline[3]   ))
+    l=[]
+    time=date+stime
+    line=f.readline()
+    while line!="":
+        if line[0:2]=="#T":
+            arrays.append((tag,time,np.array(l)))
+            l=[]
+            tag=line[3:]
+            pos=f.tell()
+            nextline=f.readline()
+            f.seek(pos)
+            splitline=nextline.split(" ")
+            stime=timedelta(hours=int(splitline[0]),minutes=int(splitline[1]),seconds=int(splitline[2]),microseconds=int(splitline[3]   ))
+            time=date+stime
+        else:
+            splitline=line.strip().split(" ")
+            t=(timedelta(hours=int(splitline[0]),minutes=int(splitline[1]),seconds=int(splitline[2]),microseconds=int(splitline[3]))-stime).total_seconds()
+            x=[t]
+            x.extend(list(map(float,splitline[4:])))
+            l.append(x)
+        line=f.readline()
+    return arrays
+            
